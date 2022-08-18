8d04bba6737d1361e92cdf4c63175a89c3e1d4f2
Alperen Ut. YA <alperenutku2002@gmail.com>
2022-08-18 16:57:34 +0300

    revert deniyorum

--- DemirbasTakipSistemi/Controllers/AccountController.cs
+++ DemirbasTakipSistemi/Controllers/AccountController.cs
@@ -1,46 +1,24 @@
 ﻿using DemirbasTakipSistemi.Repositories;
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
-using System.Threading.Tasks;
-using DemirbasTakipSistemi.Models;
-using System.Security.Claims;
-using System.Collections.Generic;
-using Microsoft.AspNetCore.Authorization;
-using DemirbasTakipSistemi.Models.DataModel;
 
 namespace DemirbasTakipSistemi.Controllers
 {
     public class AccountController : Controller
     {
         private readonly AccountRepository accountRepository = new AccountRepository();
-        Context c = new Context();
-
-        [AllowAnonymous]
-        [HttpGet]
         public IActionResult Login()
         {
-                return View();
+            return View();
         }
-        //return redirectoaction("index",category) yerine şunu Redirect("/Category/Index"); yazsınlar.
-        ///ayrica app.UseAuthentication(); app.UseAuthorization(); middlewareleri de startupa ekleyip denesinler
-        [AllowAnonymous]
         [HttpPost]
-        public async Task<IActionResult> Login(string username, string password)
+        public IActionResult Login(string username,string password)
         {
-
-            var user = accountRepository.getUser(username, password);
-            if (user != null)
+            var user=accountRepository.getUser(username, password);
+            if(user != null)
             {
-                var claims = new List<Claim>
-                {
-                    new Claim(ClaimTypes.Name, username)
-                };
-                var useridentity = new ClaimsIdentity(claims, "Login");
-                ClaimsPrincipal principal = new ClaimsPrincipal(useridentity);
-                await HttpContext.SignInAsync(principal);
-                return Redirect("/Product/ProductList"); //????
+                return RedirectToAction("ProductList", "Product");
             }
             else
             {
@@ -49,20 +27,11 @@ namespace DemirbasTakipSistemi.Controllers
             }
 
         }
-
-        public async Task<IActionResult> LogOut()
+        public IActionResult Logout()
         {
-            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            //await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
             return RedirectToAction("Login", "Account");
         }
-
-
-        //[System.Web.Mvc.OutputCache(NoStore = true, Duration = 0, VaryByParam = "*")]
-        //public IActionResult Logout()
-        //{
-        //    //await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-        //    return RedirectToAction("Login", "Account");
-        //}
 
     }
 }
--- DemirbasTakipSistemi/Startup.cs
+++ DemirbasTakipSistemi/Startup.cs
@@ -12,9 +12,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc.Authorization;
 
 namespace DemirbasTakipSistemi
 {
@@ -31,22 +28,10 @@ namespace DemirbasTakipSistemi
         public void ConfigureServices(IServiceCollection services)
 
         {
+        
+            services.AddControllersWithViews();
 
-            //services.AddControllersWithViews();
-            services.AddMvc();
-            services.AddAuthentication(
-                CookieAuthenticationDefaults.AuthenticationScheme).
-                AddCookie(x =>
-                {
-                    x.LoginPath = "/Account/Login";
-                });
-            services.AddMvc(config =>
-            {
-                var policy = new AuthorizationPolicyBuilder()
-                                    .RequireAuthenticatedUser()
-                                    .Build();
-                config.Filters.Add(new AuthorizeFilter(policy));
-            });
+           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -68,7 +53,6 @@ namespace DemirbasTakipSistemi
             app.UseRouting();
 
             app.UseAuthorization();
-            app.UseAuthentication();
 
             app.UseEndpoints(endpoints =>
             {
